7af9c213417c0c6ba1a3858bc53554b63d283bfd
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-27 11:57:32 +0200

    make scrambler actually useful

--- lib/scrambler.rb
+++ lib/scrambler.rb
@@ -3,7 +3,13 @@ require "scrambler/version"
 module Scrambler
   class Scrambler
     def scramble(puzzle)
-      "U2 R' "*12 + "F'"
+      turns = [%w{R L}, %w{F B}, %w{D U}]
+      variants = ['', "'", '2']
+      axis = rand turns.size
+      (1..25).map do
+        axis = (axis + rand(turns.size - 1) + 1) % turns.size
+        turns[axis].sample + variants.sample
+      end.join(' ')
     end
   end
 end
