a2048e4cc333d62c73042a7e3ff9f55e179fc6bd
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-27 03:19:40 +0200

    cleanup spec

--- spec/scrambler/scrambler_spec.rb
+++ spec/scrambler/scrambler_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 
 describe Scrambler::Scrambler, "scrambling length" do
   it "should give a 25 move scramble for a 3x3x3" do
-    scrambler = Scrambler::Scrambler.new
-    scrambler.scramble(:"3x3x3").split(/ /).size.should eql(25)
+    subject.scramble(:"3x3x3").split(/ /).size.should eql(25)
   end
 end
