9eefdf9c407976ada45155046bce2e3987dd0379
Tim Habermaas <tim.habermaas@gmail.com>
2011-09-28 19:06:05 +0200

    add 2x2x2 permutation solver

--- lib/scrambler/random_state/two_by_two.rb
+++ lib/scrambler/random_state/two_by_two.rb
@@ -4,8 +4,54 @@ require "scrambler/random_state/corner_permutation"
 module Scrambler
   module RandomState
     class TwoByTwo
+      SOLVED_PERMUTATION = [0, 1, 2, 3, 4, 5, 6]
+      SOLVED_ORIENTATION = [0] * 6
+
+      def initialize
+        @permutation_map = {}
+        SOLVED_PERMUTATION.permutation.each do |p|
+          @permutation_map[p] = { :R => CornerPermutation.new(p).turn(:R),
+                                  :U => CornerPermutation.new(p).turn(:U),
+                                  :F => CornerPermutation.new(p).turn(:F)
+                                }
+        end
+      end
+
       def scramble
-        raise "Not Yet Implemented"
+        permutation = SOLVED_PERMUTATION.shuffle
+
+        solve(CornerPermutation.new(permutation), nil)
+      end
+
+      def solve(corner_permutation, corner_orientation)
+        move_limit = -1
+        begin
+          move_limit += 1
+        end while !(solution = search(corner_permutation, corner_orientation, move_limit))
+
+        solution.join " "
+      end
+
+    private
+      def search(current_permutation, current_orientation, limit, solution = [], moves = 0)
+        if current_permutation.to_a == SOLVED_PERMUTATION
+          return solution
+        elsif moves >= limit
+          return false
+        else
+          [:R, :U, :F].each do |turn|
+            unless solution.last == turn
+              perm = current_permutation.clone
+              ["", "2", "'"].each do |modifier|
+                perm.turn(turn)
+                s = search(perm, current_orientation, limit, solution + ["#{turn}#{modifier}"], moves + 1)
+                return s if s
+              end
+            end
+          end
+        end
+
+        return false
       end
     end
   end
---
+++ spec/scrambler/random_state/two_by_two_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe Scrambler::RandomState::TwoByTwo do
+  include Scrambler::RandomState
+
+  let(:valid_turns) { %w(R R' R2 U U' U2 F F' F2) }
+
+  it "should not have more than 11 moves" do
+    subject.scramble.split(" ").size.should be <= 11
+  end
+
+  it "should only contain moves out or <R, F, U>" do
+    subject.scramble.split(" ").each do |turn|
+      valid_turns.should include(turn)
+    end
+  end
+
+  it "should return different scrambles (almost) every time" do
+    subject.scramble.should_not == subject.scramble
+  end
+
+  it "should solve scramble R U optimally" do
+    subject.solve(CornerPermutation.new([3, 1, 5, 2, 0, 4, 6]), CornerOrientation.new([0, 1, 2, 0, 2, 1, 0])).should == "U' R'"
+  end
+
+  xit "should solve scramble F U R U' R' F' optimally" do
+    subject.solve(CornerPermutation.new([3, 2, 1, 0, 4, 5, 6]), CornerOrientation.new([0, 0, 2, 1, 0, 0, 0])).should == "F R U R' U' F'"
+  end
+end
